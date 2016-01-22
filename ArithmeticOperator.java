2be1e6492a7c599f04f5c467d244e3f293873ba5
Siva Koka <Siva Koka>
2016-01-21 20:02:22 -0500

    Adding my Arithmetic Java file

---
+++ ArithmeticOperator.java
@@ -0,0 +1,22 @@
+public class ArithmeticOperator {
+    
+	 public static void main(String[] args){
+	 
+	    int a = 10, b= 2;
+		
+		int c = a+b;
+		int d = a-b;
+		int e =a*b;
+		int f = a/b;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+	 
+	 
+	 }
+
+
+
+}
