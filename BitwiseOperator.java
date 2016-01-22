452ebf258051d6f8e61ec30249548056893a6f7a
Siva Koka <Siva Koka>
2016-01-21 20:56:31 -0500

    Learning github

---
+++ BitwiseOperator.java
@@ -0,0 +1,24 @@
+public class BitwiseOperator {
+
+  public static void main(String[] args){
+  
+    int a= 14, b=5, c=20, d=20;
+	
+	int x = a & b;
+	int y = a | b;
+	int z = a ^ b;
+	
+	c = c >> 1;
+	d = d << 1;
+	
+	System.out.println("bitwise And: "+ x);
+	System.out.println("bitwise OR: "+ y);	
+ 	System.out.println("bitwise XOR: "+ z);  
+ 	System.out.println("right shift (reduce the value to half) "+c);    
+ 	System.out.println("left shift ( increase the value to double) "+ d);
+  
+  }
+
+
+
+}
---
+++ CommandInputs.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class CommandInputs{
+
+   public static void main(String[] args){
+   
+   Scanner scan = new Scanner(System.in);
+   int a = scan.nextInt();
+   
+   
+   System.out.println("User entered input is "+ a);
+   
+   
+   }
+
+
+}
